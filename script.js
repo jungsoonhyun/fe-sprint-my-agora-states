847cd382df919b40a47498a4eb911e401e0ba7d4
soonhyun jung <soonhyunjung@soonhyunui-noteubug.local>
2023-01-10 15:36:26 +0900

    질문페이지

--- script.js
+++ script.js
@@ -6,16 +6,44 @@ const convertToDiscussion = (obj) => {
   const li = document.createElement("li"); // li 요소 생성
   li.className = "discussion__container"; // 클래스 이름 지정
 
-  const avatarWrapper = document.createElement("div");
-  avatarWrapper.className = "discussion__avatar--wrapper";
-  const discussionContent = document.createElement("div");
-  discussionContent.className = "discussion__content";
-  const discussionAnswered = document.createElement("div");
-  discussionAnswered.className = "discussion__answered";
+/*<img class="discussion__avatar--image"
+src="https://avatars.githubusercontent.com/u/12145019?s=64&u=5c97f25ee02d87898457e23c0e61b884241838e3&v=4"
+alt="avatar of kimploo"></img>*/
+
+const avatarWrapper = document.createElement("div");
+avatarWrapper.className = "discussion__avatar--wrapper";
+
+const avatarImg = document.createElement("img");
+avatarImg.src = obj.avatarUrl;
+avatarImg.alt = 'avatar of' + obj.author;
+
+avatarWrapper.append(avatarImg);
+
+
+const discussionContent = document.createElement("div");
+discussionContent.className = "discussion__content";
+const discussionAnswered = document.createElement("div");
+discussionAnswered.className = "discussion__answered";
 
   // TODO: 객체 하나에 담긴 정보를 DOM에 적절히 넣어주세요.
 
+const discussionTitle = document.createElement("h2");
+const titleAnchor = document.createElement("a");
+titleAnchor.href = obj.url;
+titleAnchor.textContent = obj.title;
+discussionTitle.append(titleAnchor);
 
+const discussionInformation = document.createElement("div");
+discussionInformation.className = "discussion_information";
+discussionInformation.textContent = `${obj.author} / 
+${new Date(obj.createdAt).toLocaleTimeString()}`;
+discussionContent.append(discussionTitle, discussionInformation);
+
+const checked = document.createElement("p");
+/*checked.textContent = obj.answer ?  : ;*/
+discussionAnswered.append(checked);
+
+/*const face =*/ 
 
   li.append(avatarWrapper, discussionContent, discussionAnswered);
   return li;
